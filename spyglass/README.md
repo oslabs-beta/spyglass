03052edbd324087e7f93e13e53c7aab4f0b6f9e8
cindychau1 <cindyxchau1@gmail.com>
2023-03-07 23:07:44 -0500

    boilerplate setup

---
+++ spyglass/README.md
@@ -0,0 +1,4 @@
+# spyglass
+A kubernetes metrics GUI
+
+Team: Anthony Vega, Cindy Chau, Easton Miller, and Alex Czaja
---
+++ spyglass/vite.config.js
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 8080,
+    hmr: {
+      host: '0.0.0.0',
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      },
+    },
+  },
+});
