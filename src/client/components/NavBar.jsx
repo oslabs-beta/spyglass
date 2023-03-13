502bd02c585b3b2e43f73f9c98a52432484e4a99
Alex C <alexc@briank.lan>
2023-03-13 13:03:34 -0600

    app page now rendering

--- src/client/components/NavBar.jsx
+++ src/client/components/NavBar.jsx
@@ -1,18 +1,14 @@
 import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Button from '@mui/material/Button';
+import { AppBar, Toolbar, Button } from '@mui/material';
 
-export default function NavBar() {
+function NavBar() {
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          {/* Add Spyglass logo */}
-          <Button color="inherit">Logout</Button>
-        </Toolbar>
-      </AppBar>
-    </Box>
+    <AppBar className="NavBar" position="static" sx={{ bgcolor: 'black' }}>
+      <Toolbar>
+        <Button color="inherit">Logout</Button>
+      </Toolbar>
+    </AppBar>
   );
 }
+
+export default NavBar;
