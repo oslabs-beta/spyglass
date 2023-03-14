d25901f494a544aae7ed1f462eb71600d611b562
Alex C <alexc@briank.lan>
2023-03-13 18:38:44 -0600

    started routes for app pages

--- src/client/pages/App.jsx
+++ src/client/pages/App.jsx
@@ -2,6 +2,9 @@ import { useState } from 'react';
 import NavBar from '../components/NavBar';
 import SideBar from '../components/SideBar';
 import MetricPanel from '../components/MetricPanel';
+import ClusterMetricsDashboard from '../components/dashboards/ClusterMetricsDashboard';
+import CostAnalysisDashboard from '../components/dashboards/CostAnalysisDashboard';
+import { Routes, Route } from 'react-router-dom';
 import '../styles/App.css';
 
 function App() {
@@ -9,7 +12,10 @@ function App() {
     <div className="App">
       <NavBar />
       <SideBar />
-      {/* <MetricPanel /> */}
+      <Routes>
+        <Route path="/cluster" element={<ClusterMetricsDashboard />} />
+        <Route path="/cost" element={<CostAnalysisDashboard />} />
+      </Routes>
     </div>
   );
 }
