68630021085f995caf7d6d51a38487170e32ed6d
Cindy Chau <cindyxchau1@gmail.com>
2023-03-14 12:59:44 -0400

    added spyglass-values.yaml file

--- src/client/pages/App.jsx
+++ src/client/pages/App.jsx
@@ -4,7 +4,7 @@ import SideBar from '../components/SideBar';
 import MetricPanel from '../components/MetricPanel';
 import ClusterMetricsDashboard from '../components/dashboards/ClusterMetricsDashboard';
 import CostAnalysisDashboard from '../components/dashboards/CostAnalysisDashboard';
-import { Routes, Route } from 'react-router-dom';
+// import { Routes, Route } from 'react-router-dom';
 import '../styles/App.css';
 
 function App() {
@@ -12,10 +12,10 @@ function App() {
     <div className="App">
       <NavBar />
       <SideBar />
-      <Routes>
+      {/* <Routes>
         <Route path="/cluster" element={<ClusterMetricsDashboard />} />
         <Route path="/cost" element={<CostAnalysisDashboard />} />
-      </Routes>
+      </Routes> */}
     </div>
   );
 }
