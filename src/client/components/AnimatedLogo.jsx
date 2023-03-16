f3ae7c7bf351a39ad9e8b7dea6575111cf2e6fef
Cindy Chau <cindyxchau1@gmail.com>
2023-03-16 12:11:37 -0400

    modified animated logo

--- src/client/components/AnimatedLogo.jsx
+++ src/client/components/AnimatedLogo.jsx
@@ -4,7 +4,10 @@ import { motion } from 'framer-motion';
 
 function AnimatedLogo() {
   return (
-    <motion.div whileHover={{ scale: 0.9, rotate: 360 }}>
+    <motion.div
+      whileHover={{ scale: 1, rotate: 25 }}
+      whileTap={{ scale: 1, rotate: -25 }}
+    >
       <img src={spyglass} className="spyglass-logo" alt="spyglass-logo" />
     </motion.div>
   );
