30dbc43025bb9e918ae749291913ceb5edaf96e1
Cindy Chau <cindyxchau1@gmail.com>
2023-03-21 09:48:25 -0400

    added fetch request for kubecost and parsed data

--- src/client/components/dashboards/CostAnalysisDashboard.jsx
+++ src/client/components/dashboards/CostAnalysisDashboard.jsx
@@ -1,22 +1,40 @@
 import { useState, useEffect } from 'react';
-import { terminal } from 'virtual:terminal';
+// import { terminal } from 'virtual:terminal';
 
 const costURL = [
   'http://localhost:9090/model/allocation?window=15d&aggregate=cluster'
 ];
 
 function CostAnalysisDashboard() {
-  // const [costs, setCosts] = useState({});
+  const [costs, setCosts] = useState({});
+  // initialize cost categories
+  let totalCPU = 0;
+  let totalRAM = 0;
+  let totalPV = 0;
   // fetch cost data from Kubecost
-  fetch(costURL, {
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  })
+  fetch(costURL)
     .then((res) => res.json())
-    .then((data) => terminal.log(data.data))
+    .then((data) => {
+      costsArr = data.data;
+      window.console.log(costsArr);
+    })
     .catch((err) => terminal.log('error in fetching cost data: ', err));
+  // parse through fetched data
+  for (const metricObj of costsArr) {
+    for (const cluster in metricObj) {
+      if (cluster[key] === 'cpuCost') totalCPU += metricObj['cpuCost'];
+      if (cluster[key] === 'ramCost') totalRAM += metricObj['ramCost'];
+      if (cluster[key] === 'pvCost') totalPV += metricObj['pvCost'];
+    }
+  }
+  const costsData = {
+    'CPU Cost': totalCPU,
+    'RAM Cost': totalRAM,
+    'PV Cost': totalPV
+  };
+  window.console.log(costsData);
+  setCosts(costsData);
   return <h1 className="cluster">Cost Dashboard</h1>;
 }
-
+console.log('hello');
 export default CostAnalysisDashboard;
