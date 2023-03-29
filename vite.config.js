cca2ed5620e89ce74d8e9c8398d967ccb486f562
Cindy Chau <cindyxchau1@gmail.com>
2023-03-29 12:59:09 -0400

    modified vite config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -14,11 +13,4 @@ export default defineConfig({
       }
     }
   }
-  // build: {
-  //   rollupOptions: {
-  //     input: {
-  //       main: resolve(__dirname, 'index.html')
-  //     }
-  //   }
-  // }
 });
